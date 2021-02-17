dc79496a65618193d659a6d183c7f79806a6d6e2
britafolkvord <brita.folk@hotmail.com>
2021-02-17 18:45:02 +0100

    update imports and styles

--- brita-folkvord-js-frameworks-ca-master/react-ca/src/components/layout/layout.jsx
+++ brita-folkvord-js-frameworks-ca-master/react-ca/src/components/layout/layout.jsx
@@ -1,44 +1,39 @@
-import React from "react";
-import {Navbar, Nav, Container} from "react-bootstrap";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from "react-router-dom";
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 
-import Home from "../home/home";
-import Contact from "../contact/contact";
-import GameDetails from "../game/gameDetails";
+import Home from '../home/home';
+import Contact from '../contact/contact';
+import GameDetails from '../game/gameDetails';
 
 function Layout() {
-  return (
-    <Router>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <NavLink to="/" exact>
-          <Navbar.Brand>Games</Navbar.Brand>
-        </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavLink to="/" exact className="nav-link">
-              Home
-            </NavLink>
-            <NavLink to="/contact" className="nav-link">
-              Contact
-            </NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Container>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/game/:id" component={GameDetails} />
-        </Switch>
-      </Container>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <NavLink to="/" exact>
+                    <Navbar.Brand>Games</Navbar.Brand>
+                </NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavLink to="/" exact className="nav-link">
+                            Home
+                        </NavLink>
+                        <NavLink to="/contact" className="nav-link">
+                            Contact
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className="bodyContainer">
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/game/:id" component={GameDetails} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default Layout;
