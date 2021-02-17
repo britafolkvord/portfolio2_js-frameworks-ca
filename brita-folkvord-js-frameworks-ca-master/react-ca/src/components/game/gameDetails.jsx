8a522803fbf6cf31008b67cb247a668026bf9fd7
britafolkvord <brita.folk@hotmail.com>
2021-02-17 18:45:59 +0100

    imports, error handling and styles

--- brita-folkvord-js-frameworks-ca-master/react-ca/src/components/game/gameDetails.jsx
+++ brita-folkvord-js-frameworks-ca-master/react-ca/src/components/game/gameDetails.jsx
@@ -1,48 +1,69 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import {Spinner, Row, Col, Image, Container} from "react-bootstrap";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Image, Spinner, Button } from 'react-bootstrap';
 
-import { BASE_URL } from "../constants/api";
-import Heading from "../layout/heading";
+import { BASE_URL } from '../constants/api';
+import Heading from '../layout/heading';
+
+const Status = {
+    Loading: 0,
+    Success: 1,
+    Error: 2,
+};
 
 function GameDetails() {
-  const [detail, setDetail] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [detail, setDetail] = useState([]);
+    const [status, setStatus] = useState(Status.Loading);
 
-  let { id } = useParams();
+    let { id } = useParams();
 
-  const url = BASE_URL + "/" + id;
+    const url = BASE_URL + '/' + id;
 
+    useEffect(() => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                if (json.error) {
+                    throw new Error('Error fetching specific game');
+                } else {
+                    setDetail(json);
+                    setStatus(Status.Success);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                setStatus(Status.Error);
+            });
+    }, [url]);
 
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => setDetail(json))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
-  }, [url]);
-
-  if (loading) {
-    return <Spinner animation="border" className="spinner" />;
-  }
-
-  return (
-    <Container fluid>
-      <Heading title="Game Details" />
-      <Row className="details">
-        <Col md={6} className="detail-image">
-          <Image src={detail.background_image} />
-        </Col>
-        <Col className="detail-info">
-          <h2>{detail.name}</h2>
-          <p>
-            <b>Description:</b> {detail.description}
-          </p>
-          <a href={detail.website}>Go to website</a>
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <div className="detailsContainer">
+            {status === Status.Error ? <h2>Something went wrong fetching games. Try refreshing the page</h2> : null}
+            {status === Status.Loading ? <Spinner animation="border" className="spinner" /> : null}
+            {status === Status.Success ? (
+                <>
+                    <Heading title={detail.name} />
+                    <div className="details">
+                        <div className="detail-image">
+                            <Image src={detail.background_image} />
+                        </div>
+                        <div className="detail-info">
+                            <p>
+                                <b>Description:</b> {detail.description}
+                            </p>
+                            <div className="btnContainer">
+                                <Button>
+                                    <a href={detail.website} className="link">
+                                        Go to website
+                                    </a>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ) : null}
+        </div>
+    );
 }
 
 export default GameDetails;
--- brita-folkvord-js-frameworks-ca-master/react-ca/src/components/game/gameList.jsx
+++ brita-folkvord-js-frameworks-ca-master/react-ca/src/components/game/gameList.jsx
@@ -1,70 +1,84 @@
-import React, { useState, useEffect } from "react";
-import {Spinner, Button, Card, ListGroup, Col, Row, Container} from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Spinner, Button, Card, ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-import GameSearch from "./searchGames";
-import { BASE_URL } from "../constants/api";
+import GameSearch from './searchGames';
+import { BASE_URL } from '../constants/api';
+
+const Status = {
+    Loading: 0,
+    Success: 1,
+    Error: 2,
+};
 
 function GameList() {
-  const [games, setGames] = useState([]);
-  const [filteredGames, setFilteredGames] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [games, setGames] = useState([]);
+    const [filteredGames, setFilteredGames] = useState([]);
+    const [status, setStatus] = useState(Status.Loading);
 
-  useEffect(() => {
-    fetch(BASE_URL)
-      .then((response) => response.json())
-      .then((json) => {
-        setGames(json.results);
-        setFilteredGames(json.results);
-      })
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
-  }, []);
+    useEffect(() => {
+        fetch(BASE_URL)
+            .then((response) => response.json())
+            .then((json) => {
+                if (json.error) {
+                    throw new Error('Error fetching games');
+                } else {
+                    setGames(json.results);
+                    setFilteredGames(json.results);
+                    setStatus(Status.Success);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                setStatus(Status.Error);
+            });
+    }, []);
 
-  const filterGames = function (e) {
-    const searchValue = e.target.value.toLowerCase();
-    const filteredArray = games.filter(function (game) {
-      const lowerCaseName = game.name.toLowerCase();
-      if (lowerCaseName.includes(searchValue)) {
-        return true;
-      }
-      return false;
-    });
-    setFilteredGames(filteredArray);
-  };
+    const filterGames = function (e) {
+        const searchValue = e.target.value.toLowerCase();
+        const filteredArray = games.filter(function (game) {
+            const lowerCaseName = game.name.toLowerCase();
+            if (lowerCaseName.includes(searchValue)) {
+                return true;
+            }
+            return false;
+        });
+        setFilteredGames(filteredArray);
+    };
 
-  if (loading) {
-    return <Spinner animation="border" className="spinner" />;
-  }
-
-  return (
-    <Container fluid>
-      <GameSearch handleSearch={filterGames} />
-      <Row>
-        {filteredGames.map((game) => {
-          const { id, name, background_image, rating, released } = game;
-
-          return (
-            <Col sm={6} md={3} key={id}>
-              <Card style={{ minWidth: "18rem" }}>
-                <Card.Img variant="top" src={background_image} />
-                <Card.Body>
-                  <Card.Title>{name}</Card.Title>
-                  <ListGroup variant="flush">
-                    <ListGroup.Item>Rating: {rating}</ListGroup.Item>
-                    <ListGroup.Item>Released: {released}</ListGroup.Item>
-                  </ListGroup>
-                  <Link to={"../game/" + id}>
-                    <Button variant="primary">Game Details</Button>
-                  </Link>
-                </Card.Body>
-              </Card>
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
-  );
+    return (
+        <div className="lol">
+            <GameSearch handleSearch={filterGames} />
+            {status === Status.Error ? <h2>Something went wrong fetching games. Try refreshing the page</h2> : null}
+            {status === Status.Loading ? <Spinner animation="border" className="spinner" /> : null}
+            <div className="cardsContainer">
+                {filteredGames.map((game) => {
+                    const { id, name, background_image, rating, released } = game;
+                    return (
+                        <React.Fragment key={id}>
+                            {status === Status.Success ? (
+                                <Card>
+                                    <Card.Img variant="top" src={background_image} />
+                                    <Card.Body>
+                                        <Card.Title>{name}</Card.Title>
+                                        <ListGroup variant="flush">
+                                            <ListGroup.Item>Rating: {rating}</ListGroup.Item>
+                                            <ListGroup.Item>Released: {released}</ListGroup.Item>
+                                        </ListGroup>
+                                        <Link to={'../game/' + id}>
+                                            <Button variant="primary" block>
+                                                Game Details
+                                            </Button>
+                                        </Link>
+                                    </Card.Body>
+                                </Card>
+                            ) : null}
+                        </React.Fragment>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default GameList;
